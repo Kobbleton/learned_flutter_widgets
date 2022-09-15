3e5898f657f28c50948fc07d1121151a01cc61d6
Vitaly Shastun <kobbleton@gmail.com>
2022-09-15 17:16:00 +0530

    Add About Dialog

---
+++ about_dialog/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) => const AboutDialog(
+              applicationIcon: FlutterLogo(),
+              applicationLegalese: 'Legalese',
+              applicationName: 'Flutter App',
+              applicationVersion: 'version 1.0.0',
+              children: [
+                Text('This is a sample text for the About Dialog Widget')
+              ],
+            ),
+          );
+        },
+        child: const Text('Show About Dialog'),
+      ),
+    );
+  }
+}
