349aafa7743fbc24d7dc9a0299325de0fd25c67b
Vitaly Shastun <kobbleton@gmail.com>
2022-09-21 12:05:00 +0530

    Add Align Widget
    Add Align Widget

---
+++ align_widget/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData.dark(),
+    debugShowCheckedModeBanner: false,
+    home: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Align'),
+      ),
+      body: Center(
+        child: Container(
+          height: 120.0,
+          width: double.infinity,
+          color: Colors.blueGrey,
+          child: const Align(
+            //position
+            alignment: Alignment.center,
+            child: FlutterLogo(size: 60),
+          ),
+        ),
+      ),
+    );
+  }
+}
