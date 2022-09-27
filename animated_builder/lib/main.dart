3c336eddbab876bdc039f4e19f2515d0fdf3d45a
Vitaly Shastun <kobbleton@gmail.com>
2022-09-27 19:17:27 +0530

    Animated Builder Update
    Animated Builder Update

--- animated_builder/lib/main.dart
+++ animated_builder/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'dart:math' as math;
 
 void main() {
   runApp(MaterialApp(
@@ -17,18 +17,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 10),
+    vsync: this,
+  )..repeat();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: '',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(''),
-        ),
-        body: const Center(
-          child: Text(''),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animated Builder'),
+      ),
+      body: Center(
+        child: AnimatedBuilder(
+            animation: _controller,
+            child: const FlutterLogo(size: 100),
+            builder: (BuildContext context, Widget? child) {
+              return Transform.rotate(
+                angle: _controller.value * 2.0 * math.pi,
+                child: child,
+              );
+            }),
       ),
     );
   }
