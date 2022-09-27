dc1df4dc5bfa9a08ba1e41ca33fe057fa660acd2
Vitaly Shastun <kobbleton@gmail.com>
2022-09-27 18:04:18 +0530

    Add Animated Default Text Style
    Add Animated Default Text Style widget

---
+++ about_dialog/animated_default_text_style/android/app/src/main/kotlin/com/example/animated_default_text_style/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animated_default_text_style
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ about_dialog/animated_default_text_style/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData.dark(),
+    debugShowCheckedModeBanner: false,
+    home: const MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _first = true;
+  double _fontSize = 60;
+  Color _color = Colors.blue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animated Default Text Style'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 120,
+              child: AnimatedDefaultTextStyle(
+                duration: const Duration(milliseconds: 300),
+                style: TextStyle(
+                  fontSize: _fontSize,
+                  color: _color,
+                  fontWeight: FontWeight.bold,
+                ),
+                child: const Text('Flutter'),
+              ),
+            ),
+            TextButton(
+              onPressed: (() {
+                setState(() {
+                  _fontSize = _first ? 90 : 60;
+                  _color = _first ? Colors.blue : Colors.red;
+                  _first = !_first;
+                });
+              }),
+              child: const Text('Switch'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
