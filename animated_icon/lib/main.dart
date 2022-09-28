e3508bf897a01eb36cd7c75a3c747c338b8c0550
Vitaly Shastun <kobbleton@gmail.com>
2022-09-28 19:02:15 +0530

    Add Animated Icon
    Add Animated Icon Widget

---
+++ animated_icon/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData.dark(),
+    debugShowCheckedModeBanner: false,
+    home: const MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
+  bool isPlay = false;
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            if (isPlay == false) {
+              //.forward means next icon
+              _controller.forward();
+              isPlay = true;
+            } else {
+              //.reverse means previous icon
+              _controller.reverse();
+              isPlay = false;
+            }
+          },
+          child: AnimatedIcon(
+            icon: AnimatedIcons.play_pause,
+            progress: _controller,
+            size: 100,
+          ),
+        ),
+      ),
+    );
+  }
+}
