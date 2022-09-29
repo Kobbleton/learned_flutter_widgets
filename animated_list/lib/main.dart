02e6a542e086b30c47e2e5bd13ca041fee078e24
Vitaly Shastun <kobbleton@gmail.com>
2022-09-29 15:52:28 +0530

    Add Animated List
    Add Animated List  Widget

---
+++ animated_list/lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData.dark(),
+    debugShowCheckedModeBanner: false,
+    home: const MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final List _items = [];
+  final GlobalKey<AnimatedListState> _key = GlobalKey();
+
+  void _addItem() {
+    _items.insert(0, 'Item ${_items.length + 1}');
+    _key.currentState!.insertItem(
+      0,
+      duration: const Duration(seconds: 1),
+    );
+  }
+
+  void _removeItem(int index) {
+    _key.currentState!.removeItem(
+      index,
+      (_, animation) {
+        return SizeTransition(
+          sizeFactor: animation,
+          child: const Card(
+            margin: EdgeInsets.all(10),
+            color: Colors.red,
+            child: ListTile(
+              title: Text(
+                'Deleted',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+          ),
+        );
+      },
+      duration: const Duration(milliseconds: 300),
+    );
+    _items.removeAt(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0XFF102436),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1C3E5D),
+        title: const Text('Animated List Tile'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            IconButton(
+              onPressed: _addItem,
+              icon: const Icon(Icons.add),
+            ),
+            Expanded(
+              child: AnimatedList(
+                key: _key,
+                initialItemCount: 0,
+                padding: const EdgeInsets.all(10),
+                itemBuilder: (context, index, animation) {
+                  return SizeTransition(
+                    sizeFactor: animation,
+                    key: UniqueKey(),
+                    child: Card(
+                      margin: const EdgeInsets.all(10),
+                      color: Colors.orangeAccent,
+                      child: ListTile(
+                        title: Text(
+                          _items[index],
+                          style: const TextStyle(fontSize: 24),
+                        ),
+                        trailing: IconButton(
+                            onPressed: () {
+                              _removeItem(index);
+                            },
+                            icon: const Icon(Icons.delete)),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
