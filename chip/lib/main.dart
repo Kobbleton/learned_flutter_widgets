fbe86ebfb62d5a7beeb6ae89eb3ea63c80726f26
Vitaly Shastun <kobbleton@gmail.com>
2022-10-03 18:16:12 +0530

    Add Chip
    Add Chip Widget

---
+++ chip/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData.dark(),
+    debugShowCheckedModeBanner: false,
+    home: const MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0XFF102436),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1C3E5D),
+        title: const Text(''),
+      ),
+      body: Center(
+        child: Chip(
+          label: const Text('This is a Flutter chip'),
+          onDeleted: () => debugPrint('Do Something'),
+        ),
+      ),
+    );
+  }
+}
