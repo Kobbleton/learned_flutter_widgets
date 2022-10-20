f3292823c26fbcd56e502e4474384907fb9281b6
Vitaly Shastun <kobbleton@gmail.com>
2022-10-20 18:38:54 +0530

    Add Cupertino Scrollbar
    Add Cupertino Scrollbar Widget

---
+++ cupertino_scrollbar/lib/main.dart
@@ -0,0 +1,49 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData.dark(),
+    debugShowCheckedModeBanner: false,
+    home: const MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0XFF102436),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1C3E5D),
+        title: const Text(''),
+      ),
+      body: CupertinoScrollbar(
+        thickness: 6,
+        thicknessWhileDragging: 10,
+        radius: const Radius.circular(34),
+        radiusWhileDragging: Radius.zero,
+        child: ListView.builder(
+          itemCount: 50,
+          itemBuilder: (context, index) {
+            return Center(
+              child: Text(
+                '$index',
+                style: const TextStyle(fontSize: 30),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
