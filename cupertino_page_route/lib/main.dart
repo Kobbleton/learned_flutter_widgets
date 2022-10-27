f692ea077cd4dfe19aac1cad98099e8135f48487
Vitaly Shastun <kobbleton@gmail.com>
2022-10-27 16:21:19 +0530

    Add Cupertino Page Route
    Add Cupertino Page Route Widget

---
+++ cupertino_page_route/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData.dark(),
+    debugShowCheckedModeBanner: false,
+    home: const MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0XFF102436),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1C3E5D),
+        title: const Text('Page 1'),
+      ),
+      body: Center(
+        child: CupertinoButton.filled(
+          onPressed: () => Navigator.of(context).push(
+            CupertinoPageRoute(
+              builder: (context) {
+                return const PageTwo();
+              },
+            ),
+          ),
+          child: const Text('Click to page 2'),
+        ),
+      ),
+    );
+  }
+}
+
+class PageTwo extends StatelessWidget {
+  const PageTwo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0XFF102436),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1C3E5D),
+        title: const Text('Page Two'),
+      ),
+      body: const Center(
+        child: Text('Page Two'),
+      ),
+    );
+  }
+}
