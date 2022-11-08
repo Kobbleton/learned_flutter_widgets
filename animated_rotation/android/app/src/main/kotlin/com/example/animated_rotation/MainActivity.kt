0b1112446ed1e87db195f1b056598e9c1346949d
Vitaly Shastun <kobbleton@gmail.com>
2022-11-08 19:59:17 +0530

    Add Animated Rotation
    Add Animated Rotation Widget

---
+++ animated_rotation/android/app/src/main/kotlin/com/example/animated_rotation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animated_rotation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ animated_rotation/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData.dark(),
+    debugShowCheckedModeBanner: false,
+    home: const MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double turns = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0XFF102436),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1C3E5D),
+        title: const Text('Animated Rotation'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(50),
+              child: AnimatedRotation(
+                turns: turns,
+                duration: const Duration(seconds: 1),
+                child: const FlutterLogo(
+                  size: 100,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() => turns += 1 / 4);
+              },
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orangeAccent),
+              child: const Text('Rotate Logo'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
