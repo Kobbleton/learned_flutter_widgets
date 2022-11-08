9fcfce92c7f3c73fb2a500ead835844d35ff1afb
Vitaly Shastun <kobbleton@gmail.com>
2022-11-08 19:58:28 +0530

    Add Drawer + Drawer Header
    Add Drawer + Drawer Header Widget

---
+++ drawer/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData.dark(),
+    debugShowCheckedModeBanner: false,
+    home: const MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0XFF102436),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1C3E5D),
+        title: const Text('Drawer + Header'),
+      ),
+      body: Center(
+        child: Builder(
+          builder: (context) => ElevatedButton(
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            child: const Text('Open Drawer'),
+          ),
+        ),
+      ),
+      drawer: Drawer(
+          child: ListView(
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('Header'),
+          ),
+          ListTile(
+            title: const Text("Item 1"),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Item 2'),
+            onTap: () {},
+          )
+        ],
+      )),
+    );
+  }
+}
